b8ee1a91e2cdc552ba96d5bcbab45eaa50f6dc41
Yudi Andela <yudhi.andhela@gmail.com>
2024-02-17 07:00:58 +0700

    mengubah state hover text css

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
 $classes = ($active ?? false)
             ? 'flex gap-2 items-center px-1 text-white font-semibold text-sm leading-5 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'flex gap-2 items-center px-1 text-gray-300 hover:text-white text-sm leading-5 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            : 'flex gap-2 items-center px-1 text-gray-300 text-sm leading-5 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
